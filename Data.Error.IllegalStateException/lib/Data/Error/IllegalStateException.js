94724ad586b9291ded185df1b464b760231581e6
daisuke-maki <daisuke-maki@73836700-649d-af45-a754-cc59ef0abc59>
2006-07-31 02:46:47 +0000

    Imported Data.Error.IllegalStateException ver. 0.0.0.
    git-svn-id: https://jsthread.svn.sourceforge.net/svnroot/jsthread@34 73836700-649d-af45-a754-cc59ef0abc59

---
+++ Data.Error.IllegalStateException/lib/Data/Error/IllegalStateException.js
@@ -0,0 +1,10 @@
+//@esmodpp
+//@version 0.0.0
+
+//@namespace Data.Error
+//@require   Data.Error
+
+
+//@export IllegalStateException
+var IllegalStateException = newExceptionClass(NAMESPACE + ".IllegalStateException");
+
