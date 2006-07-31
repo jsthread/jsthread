f786b6cd0521b725782cfefb9579dfe72a24c050
daisuke-maki <daisuke-maki@73836700-649d-af45-a754-cc59ef0abc59>
2006-07-31 17:20:53 +0000

    Still-under-construction version of the parser. Speeded up Unicode class matching. Added lib/Concurrent/Thread/Compiler/CharacterClass.js.
    git-svn-id: https://jsthread.svn.sourceforge.net/svnroot/jsthread@36 73836700-649d-af45-a754-cc59ef0abc59

--- Concurrent.Thread/trunk/UCD_category_to_js.pl
+++ Concurrent.Thread/trunk/UCD_category_to_js.pl
@@ -32,24 +32,38 @@ for ( my $i=0;  $i < $#line;  $i++ ) {
 }
 
 print "// #generated# Last update: ",
-      DateTime::Format::Mail->format_datetime(DateTime->now(time_zone=>"GMT")), "\n";
+      DateTime::Format::Mail->format_datetime(DateTime->now(time_zone=>"Japan")), "\n";
 
-foreach ( grep{ $_->[LEFT] != $_->[RIGHT] } @line ) {
-    printf "if ( 0x%X <= c && c <= 0x%X ) return true;\n", $_->[LEFT], $_->[RIGHT];
-}
-
-print "switch ( c ) {\n";
-
-foreach ( grep{ $_->[LEFT] == $_->[RIGHT] } @line ) {
-    printf "case 0x%X:\n", $_->[LEFT];
-}
-
-print <<HERE;
-    return true;
-default:
-    return false;
-}
+(my $exp = make_exp(@line));# =~ s/\n/\n       /g;
+print <<"HERE";
+return $exp;
 // #/generated#
 HERE
 
 
+sub make_exp {
+    if ( @_ == 0 ) {
+        return "false";
+    }
+    elsif ( @_ == 1 ) {
+        if ( $_[0]->[LEFT] == $_[0]->[RIGHT] ) {
+            return sprintf "c === 0x%X", $_[0]->[LEFT];
+        }
+        else {
+            if ( defined $_[0]->[RIGHT] ) {
+                return sprintf "0x%X <= c && c <= 0x%X", $_[0]->[LEFT], $_[0]->[RIGHT];
+            }
+            else {
+                return sprintf "0x%X <= c", $_[0]->[LEFT];
+            }
+        }
+    }
+    else {
+        my $d = int($#_ / 2);
+        my $p = sprintf "c <= 0x%X", $_[$d]->[RIGHT];
+        $_[$d]->[RIGHT] = undef;
+        (my $t = make_exp(@_[0    .. $d ]));# =~ s/^/  /mg;
+        (my $f = make_exp(@_[$d+1 .. $#_]));# =~ s/^/  /mg;
+        return "$p ?\n$t :\n$f";
+    }
+}
