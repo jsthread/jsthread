8d4633f62424bab38510321c1112a1e06a3b5fe3
daisuke-maki <daisuke-maki@73836700-649d-af45-a754-cc59ef0abc59>
2006-08-05 15:53:50 +0000

    Renamed Data.Error.IllegalStateException to Data.Error.IllegalStateError.
    git-svn-id: https://jsthread.svn.sourceforge.net/svnroot/jsthread@40 73836700-649d-af45-a754-cc59ef0abc59

---
+++ Data.Error.IllegalStateError/lib/Data/Error/IllegalStateError.js
@@ -0,0 +1,10 @@
+//@esmodpp
+//@version 0.0.0
+
+//@namespace Data.Error
+//@require   Data.Error
+
+
+//@export IllegalStateError
+var IllegalStateError = newErrorClass(NAMESPACE + ".IllegalStateError");
+
