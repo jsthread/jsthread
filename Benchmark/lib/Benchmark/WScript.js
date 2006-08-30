70f57152b5ff6fc909379c3fe8d6edd9788852f3
daisuke-maki <daisuke-maki@73836700-649d-af45-a754-cc59ef0abc59>
2006-08-30 18:50:33 +0000

    [Benchmark] *Added WindowsScriptHost-binding (Benchmark/WScript).
    git-svn-id: https://jsthread.svn.sourceforge.net/svnroot/jsthread@68 73836700-649d-af45-a754-cc59ef0abc59

---
+++ Benchmark/lib/Benchmark/WScript.js
@@ -0,0 +1,14 @@
+//@esmodpp
+//@version 0.0.0
+//@require   Benchmark
+//@namespace Benchmark
+
+
+PRINT = function ( str ) {
+    WScript.StdOut.Write(str);
+};
+
+WARN = function ( str ) {
+    WScript.StdErr.Write(str);
+};
+
