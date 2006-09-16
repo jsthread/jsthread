bc3d257fec90a2fe0919f1904f30867aeeef98cb
daisuke-maki <daisuke-maki@73836700-649d-af45-a754-cc59ef0abc59>
2006-09-16 05:29:56 +0000

    [StdIO.WebBrowser] *Implemented resize and move consoles.
    git-svn-id: https://jsthread.svn.sourceforge.net/svnroot/jsthread@107 73836700-649d-af45-a754-cc59ef0abc59

--- StdIO/lib/StdIO/WebBrowser.js
+++ StdIO/lib/StdIO/WebBrowser.js
@@ -7,142 +7,227 @@
 //@require Math.ToInteger
 //@with-namespace Math
 
+//@require WebBrowser.GUI.DragAndDrop
+var setDnD = WebBrowser.GUI.setDragAndDrop;
 
 
-/* Construct following structure.
+
+function createConsole ( name, left, top, zIndex ) {
+    /* Construct following structure.
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <span>title</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div>console</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <table><tbody><tr>
+                            <td>
+                                <label><input type="checkbox" value="Wrap" /></label>
+                            </td>
+                            <td>
+                                <input type="button" value="Clear" />
+                            </td>
+                            <td>
+                                resize+
+                            </td>
+                        </tr></tbody></table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+     */
     
-    <table>
-        <tbody>
-            <tr>
-                <td>
-                    <span>title</span>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <div>console</div>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <table><tbody><tr>
-                        <td>
-                            <label><input type="checkbox" value="Wrap" /></label>
-                        </td>
-                        <td>
-                            <input type="button" value="Clear" />
-                        </td>
-                        <td>
-                            resize+
-                        </td>
-                    </tr></tbody></table>
-                </td>
-            </tr>
-        </tbody>
-    </table>
- */
-
-var table = document.createElement("TABLE");
-table.border      = 0;
-table.cellPadding = 0;
-table.cellSpacing = 0;
-table.style.borderWidth = 0;
-table.style.margin      = 0;
-table.style.padding     = 0;
-table.style.position    = "absolute";
-table.style.left        = "20px";
-table.style.top         = "20px";
-var tbody = table.appendChild( document.createElement("TBODY") );
-
-var title = tbody.appendChild( document.createElement("TR") )
-                 .appendChild( document.createElement("TD") )
-                 .appendChild( document.createElement("SPAN") );
-title.appendChild( document.createTextNode("StdOut+StdErr") );
-title.style.backgroundColor = "#EEEEEE";
-title.style.fontWeight = "bold";
-title.style.margin = 0;
-title.style.padding = "0.5ex";
-
-
-var console = tbody.appendChild( document.createElement("TR") )
-                   .appendChild( document.createElement("TD") )
-                   .appendChild( document.createElement("DIV") );
-console.style.backgroundColor = "#FFFFFF";
-console.style.border          = "solid 1px #EEEEEE";
-console.style.margin          = 0;
-console.style.fontFamily      = "monospace";
-console.style.whiteSpace      = "normal";
-console.style.padding         = "0.5em";
-console.style.width           = "480px";
-console.style.height          = "300px";
-console.style.overflow        = "scroll";
-
-var tools = tbody.appendChild( document.createElement("TR") )
-                 .appendChild( document.createElement("TD") );
-tools.align = "right";
-tools.style.margin          = 0;
-tools.style.padding         = 0;
-tools.style.backgroundColor = console.style.backgroundColor;
-tools.style.border          = console.style.border;
-tools.style.borderTopWidth  = 0;
-tools = tools.appendChild( document.createElement("TABLE") );
-tools.border      = 0;
-tools.cellPadding = 0;
-tools.cellSpacing = 0;
-tools.style.borderWidth = 0;
-tools.style.margin      = 0;
-tools.style.padding     = 0;
-tools = tools.appendChild( document.createElement("TBODY") )
-             .appendChild( document.createElement("TR") );
-
-if ( navigator.appName.indexOf("Microsoft") < 0 ) {  // IE can't switch display mode.
-    var label = tools.appendChild( document.createElement("TD") );
-    label.style.verticalAlign = "middle";
-    label.style.paddingRight  = "10px";
-    label = label.appendChild( document.createElement("LABEL") );
-    var wrap  = document.createElement("INPUT");
-    wrap.type = "checkbox";
-    wrap.checked = true;
-    wrap.onclick = function ( e ) {
-        if ( this.checked ) {
-            console.style.whiteSpace = "normal";
-        } else {
-            console.style.whiteSpace = "pre";
-        }
+    var table = document.createElement("TABLE");
+    table.border      = 0;
+    table.cellPadding = 0;
+    table.cellSpacing = 0;
+    table.style.borderWidth = 0;
+    table.style.margin      = 0;
+    table.style.padding     = 0;
+    table.style.position    = "absolute";
+    table.style.left        = left || "20px";
+    table.style.top         = top  || "20px";
+    table.style.zIndex      = zIndex;
+    var tbody = table.appendChild( document.createElement("TBODY") );
+    
+    var title = tbody.appendChild( document.createElement("TR") )
+                     .appendChild( document.createElement("TD") )
+                     .appendChild( document.createElement("SPAN") );
+    title.appendChild( document.createTextNode(name) );
+    title.style.backgroundColor = "#EEEEEE";
+    title.style.fontWeight = "bold";
+    title.style.margin     = 0;
+    title.style.padding    = "0.5ex";
+    title.style.cursor     = "move";
+    
+    var console = tbody.appendChild( document.createElement("TR") )
+                       .appendChild( document.createElement("TD") )
+                       .appendChild( document.createElement("DIV") );
+    console.style.backgroundColor = "#FFFFFF";
+    console.style.border          = "solid 1px #EEEEEE";
+    console.style.margin          = 0;
+    console.style.fontFamily      = "monospace";
+    console.style.whiteSpace      = "normal";
+    console.style.padding         = "0.5em";
+    console.style.width           = "480px";
+    console.style.height          = "300px";
+    console.style.overflow        = "scroll";
+    
+    var tools = tbody.appendChild( document.createElement("TR") )
+                     .appendChild( document.createElement("TD") );
+    tools.align = "right";
+    tools.style.margin          = 0;
+    tools.style.padding         = 0;
+    tools.style.backgroundColor = console.style.backgroundColor;
+    tools.style.border          = console.style.border;
+    tools.style.borderTopWidth  = 0;
+    tools = tools.appendChild( document.createElement("TABLE") );
+    tools.border      = 0;
+    tools.cellPadding = 0;
+    tools.cellSpacing = 0;
+    tools.style.borderWidth = 0;
+    tools.style.margin      = 0;
+    tools.style.padding     = 0;
+    tools = tools.appendChild( document.createElement("TBODY") )
+                 .appendChild( document.createElement("TR") );
+    
+    if ( navigator.appName.indexOf("Microsoft") < 0 ) {  // IE can't switch display mode.
+        var label = tools.appendChild( document.createElement("TD") );
+        label.style.verticalAlign = "middle";
+        label.style.paddingRight  = "10px";
+        label = label.appendChild( document.createElement("LABEL") );
+        var wrap  = document.createElement("INPUT");
+        wrap.type = "checkbox";
+        wrap.checked = true;
+        wrap.onclick = function ( e ) {
+            if ( this.checked ) {
+                console.style.whiteSpace = "normal";
+            } else {
+                console.style.whiteSpace = "pre";
+            }
+        };
+        label.appendChild( wrap );
+        label.appendChild( document.createTextNode("Wrap") );
+    }
+    
+    var clear = tools.appendChild( document.createElement("TD") );
+    clear.style.verticalAlign = "middle";
+    clear.style.paddingRight  = "10px";
+    var clearButton = document.createElement("INPUT");
+    clearButton.type = "button";
+    clearButton.value = "Clear";
+    clearButton.onclick = function ( ) {
+        console.innerHTML = "";
     };
-    label.appendChild( wrap );
-    label.appendChild( document.createTextNode("Wrap") );
+    clear.appendChild( clearButton );
+    
+    var resize = tools.appendChild( document.createElement("TD") );
+    resize.appendChild( document.createTextNode("┼") );
+    resize.style.fontSize        = "30px";
+    resize.style.fontFamily      = "monospace";
+    resize.style.backgroundColor = "#EEEEEE";
+    resize.style.borderWidth     = 0;
+    resize.style.padding         = 0;
+    resize.style.margin          = 0;
+    resize.style.cursor          = "nw-resize";
+    
+    var posX, posY;
+    setDnD(title, {
+        start: function ( e ) {
+            posX = e.clientX;
+            posY = e.clientY;
+        },
+        move: function ( e ) {
+            table.style.left = parseInt(table.style.left) + e.clientX - posX + "px";
+            table.style.top  = parseInt(table.style.top ) + e.clientY - posY + "px";
+            posX = e.clientX;
+            posY = e.clientY;
+        }
+    });
+    
+    var resizeX, resizeY;
+    setDnD(resize, {
+        start: function ( e ) {
+            resizeX = e.clientX;
+            resizeY = e.clientY;
+        },
+        move: function ( e ) {
+            console.style.width  = parseInt(console.style.width ) + e.clientX - resizeX + "px";
+            console.style.height = parseInt(console.style.height) + e.clientY - resizeY + "px";
+            resizeX = e.clientX;
+            resizeY = e.clientY;
+        }
+    });
+    
+    
+    setTimeout(function(){
+        if ( document.body ) {
+            document.body.appendChild(table);
+        } else {
+            setTimeout(arguments.callee, 1);
+        }
+    }, 0);
+    
+    return {console:console, element:table};
 }
 
-var clear = tools.appendChild( document.createElement("TD") );
-clear.style.verticalAlign = "middle";
-clear.style.paddingRight  = "10px";
-var clearButton = document.createElement("INPUT");
-clearButton.type = "button";
-clearButton.value = "Clear";
-clearButton.onclick = function ( ) {
-    console.innerHTML = "";
-};
-clear.appendChild( clearButton );
 
-var resize = tools.appendChild( document.createElement("TD") );
-resize.appendChild( document.createTextNode("┼") );
-resize.style.fontSize        = "30px";
-resize.style.fontFamily      = "monospace";
-resize.style.backgroundColor = "#EEEEEE";
-resize.style.borderWidth     = 0;
-resize.style.padding         = 0;
-resize.style.margin          = 0;
-resize.style.cursor          = "nw-resize";
+// Create windows for StdOut, StdErr, and StdOut+StdErr.
+with ( createConsole("StdOut+StdErr", "20px", "60px", 3) ) {
+    var dblConsole = console;
+    var dblElement = element;
+}
+with ( createConsole("StdOut"       , "40px", "40px", 2) ) {
+    var outConsole = console;
+    var outElement = element;
+}
+with ( createConsole("StdErr"       , "60px", "20px", 1) ) {
+    var errConsole = console;
+    var errElement = element;
+}
 
 
-setTimeout(function(){
-    if ( document.body ) {
-        document.body.appendChild(table);
+// Move forward window when clicked.
+dblElement.onmousedown = function ( ) {
+    dblElement.style.zIndex = 3;
+    if ( outElement.style.zIndex > errElement.style.zIndex ) {
+        outElement.style.zIndex = 2;
+        errElement.style.zIndex = 1;
     } else {
-        setTimeout(arguments.callee, 1);
+        outElement.style.zIndex = 1;
+        errElement.style.zIndex = 2;
     }
-}, 0);
+};
+
+outElement.onmousedown = function ( ) {
+    outElement.style.zIndex = 3;
+    if ( dblElement.style.zIndex > errElement.style.zIndex ) {
+        dblElement.style.zIndex = 2;
+        errElement.style.zIndex = 1;
+    } else {
+        dblElement.style.zIndex = 1;
+        errElement.style.zIndex = 2;
+    }
+};
+
+errElement.onmousedown = function ( ) {
+    errElement.style.zIndex = 3;
+    if ( dblElement.style.zIndex > outElement.style.zIndex ) {
+        dblElement.style.zIndex = 2;
+        outElement.style.zIndex = 1;
+    } else {
+        dblElement.style.zIndex = 1;
+        outElement.style.zIndex = 2;
+    }
+};
 
 
 
@@ -152,14 +237,24 @@ setTimeout(function(){
 Out.write = function ( /* variable args */ ) {
     var str = "";
     for ( var i=0;  i < arguments.length;  i++ ) str += arguments[i];
-    console.innerHTML += str.replace(/&/g, "&amp;")
-                            .replace(/</g, "&lt;")
-                            .replace(/>/g, "&gt;")
-                            .replace(/\r\n|\n|\r/g, "<br>");
+    str = str.replace(/&/g, "&amp;")
+             .replace(/</g, "&lt;")
+             .replace(/>/g, "&gt;")
+             .replace(/\r\n|\n|\r/g, "<br>");
+    outConsole.innerHTML += str;
+    dblConsole.innerHTML += str;
 };
 
-
-Err = Out;
+Err.write = function ( /* variable args */ ) {
+    var str = "";
+    for ( var i=0;  i < arguments.length;  i++ ) str += arguments[i];
+    str = str.replace(/&/g, "&amp;")
+             .replace(/</g, "&lt;")
+             .replace(/>/g, "&gt;")
+             .replace(/\r\n|\n|\r/g, "<br>");
+    errConsole.innerHTML += str;
+    dblConsole.innerHTML += str;
+};
 
 
 var buffer = "";
