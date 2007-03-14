5f1f706e1d72dc28c0782cb6afeac82b6c5825d9
daisuke-maki <daisuke-maki@73836700-649d-af45-a754-cc59ef0abc59>
2007-03-14 14:15:42 +0000

    [Data.Cons] *Created test directory and imported a basic test.
    git-svn-id: https://jsthread.svn.sourceforge.net/svnroot/jsthread@178 73836700-649d-af45-a754-cc59ef0abc59

---
+++ Data.Cons/trunk/t/00basic.js
@@ -0,0 +1,23 @@
+//@esmodpp
+//@require   Test.Simple
+//@namespace Test.Simple
+
+//@require Data.Cons
+//@with-namespace Data.Cons
+
+
+test(8, function(){
+    var c = cons("car", "cdr");
+    ok( String(c) === "(car . cdr)" );
+    
+    ok( c.car === car(c) );
+    ok( c.cdr === cdr(c) );
+    
+    ok( String(nil) === "nil" );
+    ok( nil !== "nil" );
+    ok( c != nil );
+    
+    var d = cons("head", c);
+    ok( String(d) === "(head . (car . cdr))");
+    ok( cdr(d) === c);
+});
